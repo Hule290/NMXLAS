35296dec322179395247181c1f507724c49fb157
Hule290 <legiahuy29102002@gmail.com>
2025-06-14 18:34:16 +0700

    Create BT4.py

---
+++ Lab3/BT4.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy.ndimage as nd
+import imageio.v2 as iio
+import matplotlib.pylab as plt
+data = iio.imread('pagoda.jpg')
+chua = data[125:320, 0:570]
+
+chua_zoom = nd.zoom(chua, (5,5,1))
+
+plt.imsave('chua_zoom.jpg', chua_zoom)
+
+plt.subplot(1,2,1)
+plt.imshow(chua)
+
+plt.subplot(1,2,2)
+plt.imshow(chua_zoom)
+
+plt.show()
