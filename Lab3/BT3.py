cf96daad70fe4838d4ffca4342c81f10ad0300d9
Hule290 <legiahuy29102002@gmail.com>
2025-06-14 18:14:47 +0700

    Update BT3.py

---
+++ Lab3/BT3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import scipy.ndimage as nd
+import imageio.v2 as iio
+import matplotlib.pylab as plt
+data = iio.imread('quang_ninh.jpg')
+nui = data[25:330, 445:655]
+thuyen = data[455:545, 490:655]
+
+nui_xoay = nd.rotate(nui, 45, reshape = True)
+thuyen_xoay = nd.rotate(thuyen, 45, reshape = True)
+
+plt.subplot(1, 2, 1)
+plt.imshow(nui_xoay)
+
+plt.subplot(1, 2, 2)
+plt.imshow(thuyen_xoay)
+
+plt.show()
