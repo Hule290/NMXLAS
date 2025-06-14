0f40d882cc166a54f5bd8f1621d2814e5625e79a
Hule290 <legiahuy29102002@gmail.com>
2025-06-14 18:38:17 +0700

    Update BT1.py

--- Lab3/BT1.py
+++ Lab3/BT1.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.ndimage as nd
 import imageio.v2 as iio
 import matplotlib.pylab as plt
-data = iio.imread('colorful-ripe-tropical-fruits.jpg')
+data = iio.imread('exercise/colorful-ripe-tropical-fruits.jpg')
 kiwiImg = data[920:1100, 390:570]
 
 shifted = nd.shift(kiwiImg, (0, 30, 0))
