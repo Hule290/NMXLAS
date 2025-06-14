9184c80dc10dbd8de784c4832d8126c3f8846c95
Hule290 <legiahuy29102002@gmail.com>
2025-06-14 19:10:19 +0700

    Update BT1.py

--- Lab3/BT1.py
+++ Lab3/BT1.py
@@ -2,14 +2,14 @@ import numpy as np
 import scipy.ndimage as nd
 import imageio.v2 as iio
 import matplotlib.pylab as plt
+
 data = iio.imread('exercise/colorful-ripe-tropical-fruits.jpg')
 kiwiImg = data[920:1100, 390:570]
 
 shifted = nd.shift(kiwiImg, (0, 30, 0))
-shifted = np.clip(shifted, 0, 255).astype(np.uint8)
 
 output = data.copy()
-output[920:1100, 420:600] = shifted  
+output[920:1100, 390:570] = shifted  
 
 plt.figure(figsize=(12, 6))
 plt.imshow(output)
